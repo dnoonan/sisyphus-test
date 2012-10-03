0fad6a1af2d4daa493f396d036db266e8035bdb7
Devon Noonan <devon.noonan@gmail.com>
2012-10-02 21:04:04 -0400

    Initial commit with sisyphus-rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SisyphusTest::Application.config.secret_token = 'a6046241b6db498f2c1adce46c4ba2808e08f8597c5a3a0c5d8c5017ec23bb28f4dd8fd558386e7f4c66ea737221902d57d771e1bf8ccdf6754a8d5388d033ec'
